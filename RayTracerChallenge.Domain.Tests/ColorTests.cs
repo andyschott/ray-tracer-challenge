588070aad12b2ca5b338582f0f387ee599ffc279
Andy Schott <andy@andyschott.com>
2021-12-19 21:30:51 -0500

    implement color operations

--- RayTracerChallenge.Domain.Tests/ColorTests.cs
+++ RayTracerChallenge.Domain.Tests/ColorTests.cs
@@ -3,6 +3,7 @@ namespace RayTracerChallenge.Domain.Tests;
 public class ColorTests
 {
     private readonly IFixture _fixture = new Fixture();
+    private readonly ColorComparer _comparer = new ColorComparer();
 
     [Fact]
     public void ColorValues()
@@ -21,4 +22,76 @@ public class ColorTests
         Assert.Equal(green, color.Green);
         Assert.Equal(blue, color.Blue);
     }    
+
+    [Fact]
+    public void AddColors()
+    {
+        var color1 = _fixture.Create<Color>();
+        var color2 = _fixture.Create<Color>();
+
+        var sum = color1.Add(color2);
+
+        var expectedSum = new Color
+        {
+            Red = color1.Red + color2.Red,
+            Green = color1.Green + color2.Green,
+            Blue = color1.Blue + color2.Blue
+        };
+
+        Assert.Equal(expectedSum, sum, _comparer);
+    }
+
+    [Fact]
+    public void SubtractColors()
+    {
+        var color1 = _fixture.Create<Color>();
+        var color2 = _fixture.Create<Color>();
+
+        var difference = color1.Subtract(color2);
+
+        var expectedDifference = new Color
+        {
+            Red = color1.Red - color2.Red,
+            Green = color1.Green - color2.Green,
+            Blue = color1.Blue - color2.Blue
+        };
+
+        Assert.Equal(expectedDifference, difference, _comparer);
+    }
+
+    [Fact]
+    public void MultiplyByScalar()
+    {
+        var color = _fixture.Create<Color>();
+        var scalar = _fixture.Create<float>();
+
+        var result = color.Multiply(scalar);
+
+        var expectedColor = new Color
+        {
+            Red = color.Red * scalar,
+            Green = color.Green * scalar,
+            Blue = color.Blue * scalar
+        };
+
+        Assert.Equal(expectedColor, result, _comparer);
+    }
+
+    [Fact]
+    public void MultiplyColors()
+    {
+        var color1 = _fixture.Create<Color>();
+        var color2 = _fixture.Create<Color>();
+
+        var product = color1.Multiply(color2);
+
+        var expectedProduct = new Color
+        {
+            Red = color1.Red * color2.Red,
+            Green = color1.Green * color2.Green,
+            Blue = color1.Blue * color2.Blue
+        };
+
+        Assert.Equal(expectedProduct, product, _comparer);
+    }
 }
--- RayTracerChallenge.Domain/Color.cs
+++ RayTracerChallenge.Domain/Color.cs
@@ -4,4 +4,52 @@ public record class Color
     public float Red { get; init; }
     public float Blue { get; init; }
     public float Green { get; init; }
+
+    public Color Add(Color other)
+    {
+        return new Color
+        {
+            Red = Red + other.Red,
+            Green = Green + other.Green,
+            Blue = Blue + other.Blue
+        };
+    }
+
+    public static Color operator +(Color color1, Color color2) => color1.Add(color2);
+
+    public Color Subtract(Color other)
+    {
+        return new Color
+        {
+            Red = Red - other.Red,
+            Green = Green - other.Green,
+            Blue = Blue - other.Blue
+        };
+    }
+
+    public static Color operator -(Color color1, Color color2) => color1.Subtract(color2);
+
+    public Color Multiply(float scalar)
+    {
+        return new Color
+        {
+            Red = Red * scalar,
+            Green = Green * scalar,
+            Blue = Blue * scalar
+        };
+    }
+
+    public static Color operator *(Color color, float scalar) => color.Multiply(scalar);
+
+    public Color Multiply(Color other)
+    {
+        return new Color
+        {
+            Red = Red * other.Red,
+            Green = Green * other.Green,
+            Blue = Blue * other.Blue
+        };
+    }
+
+    public static Color operator *(Color color1, Color color2) => color1.Multiply(color2);
 }
