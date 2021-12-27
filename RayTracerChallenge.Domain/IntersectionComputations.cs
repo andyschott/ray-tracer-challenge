669a9615310838c8bc6b04df3241bd6510208a55
Andy Schott <andy@andyschott.com>
2021-12-27 16:00:44 -0500

    pre-compute useful info for intersections

---
+++ RayTracerChallenge.Domain/IntersectionComputations.cs
@@ -0,0 +1,40 @@
+namespace RayTracerChallenge.Domain;
+
+public class IntersectionComputations
+{
+    public decimal T { get; init; }
+    public Sphere Object { get; init; }
+    public Tuple Point { get; init; }
+    public Tuple EyeVector { get; init; }
+    public Tuple NormalVector { get; init; }
+    public bool Inside { get; init; }
+
+    public IntersectionComputations(decimal t, Sphere obj,
+        Tuple point, Tuple eyeVector, Tuple normalVector)
+    {
+        if(!point.IsPoint)
+        {
+            throw new ArgumentException($"{nameof(point)} must be a point");
+        }
+        if(!eyeVector.IsVector)
+        {
+            throw new ArgumentException($"{nameof(eyeVector)} must be a vector");
+        }
+        if(!normalVector.IsVector)
+        {
+            throw new ArgumentException($"{nameof(normalVector)} must be a vector");
+        }
+
+        T = t;
+        Object = obj;
+        Point = point;
+        EyeVector = eyeVector;
+        NormalVector = normalVector;
+
+        Inside = normalVector.DotProduct(eyeVector) < 0;
+        if(Inside)
+        {
+            NormalVector = NormalVector * -1;
+        }        
+    }
+}
